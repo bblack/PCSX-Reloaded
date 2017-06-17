b1c6065d3b2cbd6dca25c487b8337b176ab9842c
Brian Black <black@gatech.edu>
2017-06-17 16:03:06 -0400

    implement a few more gp0 commands, and comment out printf for speed

--- pcsxr/plugins/pigl/Gpu.c
+++ pcsxr/plugins/pigl/Gpu.c
@@ -37,10 +37,23 @@ static int dataReg;
 typedef struct {
   int x;
   int y;
-  int width;
-  int height;
-} displayArea_t;
-static displayArea_t displayArea;
+} vec2_t;
+static vec2_t drawingOffset;
+typedef struct {
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+} rect_t;
+static rect_t displayArea;
+static rect_t drawingArea;
+typedef struct {
+  short maskX;
+  short maskY;
+  short offsetX;
+  short offsetY;
+} textureWindowSetting_t;
+static textureWindowSetting_t textureWindowSetting;
 const static int extraWordsByCommand[] = {
   0, 0, 2, 0, 0, 0, 0, 0, // 00
   0, 0, 0, 0, 0, 0, 0, 0, // 08
@@ -164,12 +177,12 @@ GLuint makeVramTexture(void){
 // TODO: move it
 void display(void) {
   glClear(GL_COLOR_BUFFER_BIT);
-  
+
   glEnable(GL_TEXTURE_2D);
   if (!vramTexture)
     vramTexture = makeVramTexture();
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, VRAM_WIDTH, VRAM_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_SHORT_1_5_5_5_REV, (unsigned short *)psxVub);
-  
+
   glBegin(GL_POLYGON);
   glTexCoord2f(0.0, 0.0);
   glVertex3i(-1, 1, 0);
@@ -184,16 +197,16 @@ void display(void) {
 }
 
 long CALLBACK GPUopen(HWND hwndGPU) {
-  printf("GPUopen entered\n");
+  // printf("GPUopen entered\n");
   psxVub = calloc(VRAM_PIXEL_COUNT * 2, sizeof(unsigned char));
 
   if (!glInitialized) {
-    printf("initGLWindow()...\n");
+    // printf("initGLWindow()...\n");
     initGLWindow(VRAM_WIDTH, VRAM_HEIGHT);
-    printf("returned from initGLWindow()\n");
+    // printf("returned from initGLWindow()\n");
     glInitialized = TRUE;
     makeCurrentContext();
-    printf("making GL calls now...\n");
+    // printf("making GL calls now...\n");
     glViewport(0, 0, VRAM_WIDTH, VRAM_HEIGHT);
     glClearColor(0.0, 0.0, 0.0, 0.0);
   }
@@ -220,7 +233,7 @@ void CALLBACK GPUupdateLace(void) {
 ////////////////////////////////////////////////////////////////////////
 
 unsigned long CALLBACK GPUreadStatus(void) {
-  printf("GPUreadStatus\n");
+  // printf("GPUreadStatus\n");
   return STATUSREG;
 }
 
@@ -233,7 +246,7 @@ unsigned long ulStatusControl[256];
 
 void CALLBACK GPUwriteStatus(unsigned long gdata) {
   unsigned long lCommand = (gdata >> 24) & 0xff;
-  printf("GPUwriteStatus %08x\n", gdata);
+  // printf("GPUwriteStatus %08x\n", gdata);
   ulStatusControl[lCommand] = gdata;                      // store command for freezing
 
   switch (lCommand) {
@@ -254,8 +267,8 @@ void CALLBACK GPUwriteStatus(unsigned long gdata) {
       STATUSREG |= (gdata & 0b11) << 29;
       break;
     case 0x05: // Set display area location (upper-left) in vram
-      displayArea.x = gdata & 0x03ff;
-      displayArea.y = (gdata >> 10) & 0x01ff;
+      displayArea.x1 = gdata & 0x03ff;
+      displayArea.y1 = (gdata >> 10) & 0x01ff;
       break;
     case 0x06: // Set display x-range on screen TODO: do it
       break;
@@ -271,7 +284,8 @@ void CALLBACK GPUwriteStatus(unsigned long gdata) {
       }
       break;
     default:
-      printf("Unknown GP1 cmd received: %08x\n", gdata);
+      // printf("Unknown GP1 cmd received: %08x\n", gdata);
+      break;
   }
 }
 
@@ -288,7 +302,7 @@ unsigned long CALLBACK GPUreadData(void) {
 void CALLBACK GPUreadDataMem(unsigned long * pMem, int iSize) {
   for (int row=0; row<GPUWrite.height; row++) {
     for (int col=0; col<GPUWrite.height; col++) {
-      
+
     }
   }
 }
@@ -299,8 +313,8 @@ void setupWriteTextureToVram(unsigned int * buffer, unsigned int count) {
   GPUWrite.width = buffer[2] & 0xffff;
   GPUWrite.height = (buffer[2] >> 16) & 0xffff;
   treatWordsAsPixelData = true;
-  printf("Prepared CPU->VRAM: %d * %d @ (%d, %d)\n",
-         GPUWrite.width, GPUWrite.height, GPUWrite.x, GPUWrite.y);
+  // printf("Prepared CPU->VRAM: %d * %d @ (%d, %d)\n",
+  //       GPUWrite.width, GPUWrite.height, GPUWrite.x, GPUWrite.y);
 }
 
 unsigned short * getPixel(int x, int y) {
@@ -330,6 +344,24 @@ unsigned short blend15bit(unsigned short src, unsigned short dest) {
          (outB & 0x001f);
 }
 
+// TODO: speedup e.g. with memset
+// TODO: DRY with drawSingleColorRectVarSizeSemiTrans
+void drawSingleColorRectVarSizeOpaque(unsigned int * buffer, unsigned int count) {
+  int color = buffer[0] & 0xffffff; // 24-bit
+  int color15 = get15from24(color);
+  int originY = (buffer[1] >> 16) & 0xffff;
+  int originX = buffer[1] & 0xffff;
+  int height = (buffer[2] >> 16) & 0xffff;
+  int width = buffer[2] & 0xffff;
+  unsigned short * pixel;
+
+  for (int y = originY; y < originY + height; y++) {
+    for (int x = originX; x < originX + width; x++) {
+      pixel = getPixel(x, y);
+      *pixel = color15;
+    }
+  }
+}
 
 void drawSingleColorRectVarSizeSemiTrans(unsigned int * buffer, unsigned int count) {
   int color = buffer[0] & 0xffffff; // 24-bit
@@ -339,7 +371,7 @@ void drawSingleColorRectVarSizeSemiTrans(unsigned int * buffer, unsigned int cou
   int height = (buffer[2] >> 16) & 0xffff;
   int width = buffer[2] & 0xffff;
   unsigned short * pixel;
-  
+
   for (int y = originY; y < originY + height; y++) {
     for (int x = originX; x < originX + width; x++) {
       pixel = getPixel(x, y);
@@ -350,40 +382,70 @@ void drawSingleColorRectVarSizeSemiTrans(unsigned int * buffer, unsigned int cou
 
 void executeCommandWordBuffer(unsigned int buffer[256], unsigned int count) {
   unsigned int command = (buffer[0] >> 24) & 0xff;
-  
-  printf("Executing command %02x\n", command);
+
+  // printf("Executing command %02x\n", command);
   switch (command) {
     case 0x00: // noop
       break;
     case 0x01: // docs unclear - clear fifo, like GP1(01h)? clear texture cache? ??
       break;
+    case 0x02: // single-color rect, var size, opaque
+      drawSingleColorRectVarSizeOpaque(buffer, count);
+      break;
     case 0xa0: // write texture to vram
       setupWriteTextureToVram(buffer, count);
       break;
+    case 0x60: // single-color rect, var size, opaque
+      drawSingleColorRectVarSizeOpaque(buffer, count);
+      break;
     case 0x62: // single-color rect, var size, semi-trans
       drawSingleColorRectVarSizeSemiTrans(buffer, count);
+      break;
     case 0xe1: // draw mode setting
       STATUSREG &= 0xfffffc00; // copy least-sig 10 bits to status reg...
       STATUSREG |= (buffer[0] & 0x7ff);
       STATUSREG &= 0xffff7fff; // copy bit 11 to bit 15 of status reg...
-      STATUSREG |= ((buffer[0] & 0x800) << 4);
+      STATUSREG |= ((buffer[0] << 4) & 0x800);
       // TODO: handle bits 12, 13 (textured rectangle x, y flip)
       break;
+    case 0xe2: // texture window setting
+      textureWindowSetting.maskX = buffer[0] & 0x1f;
+      textureWindowSetting.maskY = (buffer[0] >> 5) & 0x1f;
+      textureWindowSetting.offsetX = (buffer[0] >> 10) & 0x1f;
+      textureWindowSetting.offsetY = (buffer[0] >> 15) & 0x1f;
+      break;
+    case 0xe3: // set drawing area x1, y1
+      drawingArea.x1 = buffer[0] & 0x3ff;
+      drawingArea.y1 = (buffer[0] >> 10) & 0x1ff;
+      break;
+    case 0xe4: // set drawing area x2, y2
+      drawingArea.x2 = buffer[0] & 0x3ff;
+      drawingArea.y2 = (buffer[0] >> 10) & 0x1ff;
+      break;
+    case 0xe5: // set drawing offset (typically within drawing area)
+      drawingOffset.x = buffer[0] & 0x3ff;
+      drawingOffset.y = (buffer[0] >> 10) & 0x3ff;
+      break;
+    case 0xe6: // mask bit setting
+      STATUSREG &= 0xffffe7ff;
+      STATUSREG |= ((buffer[0] << 11) & 0x1800);
+      break;
     default:
-      printf("Command not yet implemented: %02x\n", command);
+      // printf("Command not yet implemented: %02x\n", command);
+      break;
   }
 }
 
 // new function, used by ePSXe, for example, to write a whole chunk of data
 // This represents a number (>= 1) of words written one-by-one to GPU0--
-// For example, texture data via DMA2 following GP0(A0h) 
+// For example, texture data via DMA2 following GP0(A0h)
 void CALLBACK GPUwriteDataMem(unsigned int * pMem, int iSize) {
   unsigned int * psxVuw = (unsigned int *) psxVub;
   int wordNum = 0;
   unsigned char command;
-  
+
   if (treatWordsAsPixelData) {
-    printf("GPU0 receiving %d things (words/pixelpairs?) to write to VRAM\n", iSize);
+    // printf("GPU0 receiving %d things (words/pixelpairs?) to write to VRAM\n", iSize);
     while ((GPUWrite.currentX < GPUWrite.x + GPUWrite.width) &&
            (GPUWrite.currentY < GPUWrite.y + GPUWrite.height) &&
            (wordNum < iSize)) {
@@ -398,10 +460,10 @@ void CALLBACK GPUwriteDataMem(unsigned int * pMem, int iSize) {
     if (wordNum == iSize) { // We've read as much as we expected to
       treatWordsAsPixelData = false;
     }
-    printf("wrote %d words to vram\n", wordNum);
+    // printf("wrote %d words to vram\n", wordNum);
   } else {
     for (int i = 0; i < iSize; i++) {
-      printf("GP0(%08xh)\n", pMem[i]);
+      // printf("GP0(%08xh)\n", pMem[i]);
       commandWordsBuffer[commandWordsReceived] = pMem[i];
       if (commandWordsReceived == 0) {
         command = (pMem[i] >> 24) & 0xff;
@@ -419,7 +481,7 @@ void CALLBACK GPUwriteDataMem(unsigned int * pMem, int iSize) {
 }
 
 void CALLBACK GPUwriteData(unsigned long gdata) {
-  printf("GPUwriteData %08x\n", gdata);
+  // printf("GPUwriteData %08x\n", gdata);
   GPUwriteDataMem(&gdata, 1);
 }
 
@@ -443,19 +505,19 @@ long CALLBACK GPUgetMode(void)
 ////////////////////////////////////////////////////////////////////////
 
 long CALLBACK GPUdmaChain(unsigned long * baseAddrL, unsigned long addr) {
-  printf(">> receiving dma chain...\n");
+  // printf(">> receiving dma chain...\n");
   unsigned char *psxMem = (unsigned char *)baseAddrL;
   unsigned int *memBlockWords;
   unsigned char memBlockWordCount;
-  
+
   do {
     memBlockWords = (unsigned int *)(psxMem + addr);
     memBlockWordCount = (memBlockWords[0] >> 24) & 0xff;
     GPUwriteDataMem(memBlockWords + 1, memBlockWordCount);
     addr = memBlockWords[0] & 0x00ffffff;
   } while (addr != 0x00ffffff);
-  
-  printf("<< dma chain received\n");
+
+  // printf("<< dma chain received\n");
   return 0;
 }
 
